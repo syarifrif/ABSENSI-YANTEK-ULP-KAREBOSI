e991a463f233ac6a500c39985058e20c9f7aea7e
syarifrif <147257684+syarifrif@users.noreply.github.com>
2025-09-22 13:56:50 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// GANTI dengan URL /exec dari Web App Apps Script kamu
+window.API_URL = "https://script.google.com/macros/s/AKfycbxNkBIgKvSVLYPazOTSsf-8BUFupbGD_q2216XPyDrsuj1hsNqYb-L5R3nN-GQGntXx/exec";
