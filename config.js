694f0a3575cfde477dcf2daf3308a18a4ee1cf7e
syarifrif <147257684+syarifrif@users.noreply.github.com>
2025-09-24 01:52:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // GANTI dengan URL /exec dari Web App Apps Script kamu
-window.API_URL = "https://script.google.com/macros/s/AKfycbxNkBIgKvSVLYPazOTSsf-8BUFupbGD_q2216XPyDrsuj1hsNqYb-L5R3nN-GQGntXx/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbyOus_0gz96z6nYTqYYkV6KltE9G9f811H7QSb1xH5QhlLMO3-tinG2tpmtqKHVG6e4/exec";
